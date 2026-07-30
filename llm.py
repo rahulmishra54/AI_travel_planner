c1e3cb060f67ec983639d36472f72edf62e8437d
Rahul Mishra <rahulmishrauk7@gmail.com>
2026-07-30 08:44:02 -0700

    Initial AI Travel Planner

--- llm.py
+++ llm.py
@@ -1,14 +1,13 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
-import streamlit as st
 
 load_dotenv()
-
+print(os.getenv("GOOGLE_API_KEY"))
 
 def get_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash-lite",
-        google_api_key=st.secrets.get("GOOGLE_API_KEY") or os.getenv("GOOGLE_API_KEY"),
+        model="gemini-flash-latest",
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0.7,
     )
